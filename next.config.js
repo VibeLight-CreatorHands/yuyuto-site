3bf480e1c2d3449b7f8c1c0f8a380bfdddf5d8b2
Yuyuto000 <yukkurigame0101@gmail.com>
2025-02-10 05:07:44 +0000

    3.3.4

--- next.config.js
+++ next.config.js
@@ -1,4 +1,6 @@
-const path = require('path')
+const path = require("path");
+
+const repoName = "yuyuto-site"; // 👈 GitHubのリポジトリ名を入れる
 
 module.exports = {
   reactStrictMode: true,
@@ -6,12 +8,17 @@ module.exports = {
     // クライアントとサーバー側の両方で動作するように設定
     config.module.rules.push({
       test: /\.md$/,
-      use: 'raw-loader',
-      include: [path.resolve(__dirname, 'content')],
-    })
+      use: "raw-loader",
+      include: [path.resolve(__dirname, "content")],
+    });
 
-    return config
+    return config;
   },
-  output: 'export',
-  distDir: 'out', // GitHub Pages にデプロイするための出力先ディレクトリ
-}
+  output: "export",
+  distDir: "out", // GitHub Pages にデプロイするための出力先ディレクトリ
+  basePath: `/${repoName}`, // 👈 ここを追加
+  assetPrefix: `/${repoName}/`, // 👈 ここも追加
+  images: {
+    unoptimized: true, // GitHub Pages では画像最適化を無効化
+  },
+};
