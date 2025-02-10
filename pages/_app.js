5a7a478502cd1c025c39fc76d0c105668e08044b
Yuyuto000 <yukkurigame0101@gmail.com>
2025-02-10 05:12:06 +0000

    3.3.5

---
+++ pages/_app.js
@@ -0,0 +1,20 @@
+import '../styles/global.css'; // グローバルCSSの読み込み
+import Head from 'next/head'; // Headタグを使うためにインポート
+
+// _app.js でページごとのプロパティや設定を定義
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+        <title>My Next.js Project</title> {/* ページのタイトル */}
+        <meta name="description" content="My Next.js application" /> {/* メタデータ */}
+        <link rel="icon" href="/favicon.ico" /> {/* Favicon */}
+      </Head>
+      
+      {/* ページコンポーネントをレンダリング */}
+      <Component {...pageProps} />
+    </>
+  );
+}
+
+export default MyApp;
